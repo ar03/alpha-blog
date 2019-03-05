68bb6d02c2cc0feee9142952bd4c7fdfeee79e2e
Geralt of Rivia <alex@AlexH.local>
2019-03-04 20:49:37 -0600

    Add partials and destroy action

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -33,8 +33,15 @@ class ArticlesController < ApplicationController
 
     def show
         @article = Article.find(params[:id])
-    
     end
+
+    def destroy
+        @article = Article.find(params[:id])
+        @article.destroy
+        flash[:notice] = "Article was succesfully deleted."
+        redirect_to article_path   
+    end
+
 
     private
         def article_params
